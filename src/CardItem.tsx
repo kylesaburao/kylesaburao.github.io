58f005e054bf411d04c3caf2c1c493d11dbaeea7
Kyle Saburao <ksaburao@sfu.ca>
2021-08-16 16:47:43 -0700

    Revert "Work transfer"
    This reverts commit 12e7661872f9c6322a0cead6689ecc320f26580c.

--- src/CardItem.tsx
+++ src/CardItem.tsx
@@ -4,9 +4,9 @@ import { Card } from "react-bootstrap";
 export default function CardItem({
   title,
   description,
+  body,
   image = "",
   date = "",
-  body = [],
   width = "30em",
 }) {
   return (
@@ -17,8 +17,7 @@ export default function CardItem({
         <p>{description}</p>
         {date !== "" && <em>{date}</em>}
       </Card.Header>
-      {body.length > 0 && <Card.Body><ul>{body.map(x => <li key=></li>)}</ul></Card.Body>}
-      
+      <Card.Body>{body}</Card.Body>
     </Card>
   );
 }
