d3d5343305900e3e2035ee751c75fa2cc8b3027c
Kyle Saburao <ksaburao@sfu.ca>
2021-08-17 09:58:56 -0700

    Experience refactoring

--- src/section/Experience.tsx
+++ src/section/Experience.tsx
@@ -4,23 +4,31 @@ import { Container, Col, Row } from "react-bootstrap";
 import CardItem from "../CardItem";
 
 export default function Experience() {
+  const experience = [
+    {
+      title: "SAP",
+      description: "Concur iXp Intern",
+      date: "Starting September 2021",
+    },
+    {
+      title: "NETINT Technologies Inc.",
+      description: "Co-op Firmware Engineer",
+      date: "2020",
+    },
+  ];
+
   return (
     <Container>
       <Row className="justify-content-center">
-        <Col className="d-flex justify-content-around">
-          <CardItem
-            title={"NETINT Technologies Inc."}
-            description={"Co-op Firmware Engineer"}
-            date={"2020"}
-          />
-        </Col>
-        <Col className="d-flex justify-content-around">
-          <CardItem
-            title={"SAP"}
-            description={"iXp Intern - Concur"}
-            date={"Starting September 2021"}
-          />
-        </Col>
+        {experience.map((item) => (
+          <Col className="d-flex justify-content-around">
+            <CardItem
+              title={item.title}
+              description={item.description}
+              date={item.date}
+            ></CardItem>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
