0bd3f3774a7b037d84e489b3e83be7cd78d6616b
Kyle Saburao <ksaburao@sfu.ca>
2021-11-27 15:29:17 -0800

    add new technologies

--- src/section/Technologies.tsx
+++ src/section/Technologies.tsx
@@ -4,7 +4,6 @@ import android from "../img/icons/android.svg";
 import angular from "../img/icons/angular.svg";
 import cpp from "../img/icons/c++.svg";
 import docker from "../img/icons/docker.png";
-import gcp from "../img/icons/gcp.svg";
 import git from "../img/icons/git.svg";
 import go from "../img/icons/go.svg";
 import java from "../img/icons/java.svg";
@@ -14,27 +13,36 @@ import react from "../img/icons/react.svg";
 import regex from "../img/icons/regex.svg";
 import typescript from "../img/icons/typescript.svg";
 import wireguard from "../img/icons/wireguard.svg";
+import redux from "../img/icons/redux.svg"
+import kotlin from "../img/icons/kotlin.svg"
+import computeEngine from "../img/icons/compute_engine.svg"
+import rpi from "../img/icons/rpi.svg"
+import vultr from "../img/icons/vultr.svg"
 import chunkArrayInGroups from "../manip/arrayChunker";
 
 export default function Technologies() {
   const images = chunkArrayInGroups(
     [
       react,
+      redux,
       angular,
       typescript,
       javascript,
       python,
       cpp,
+      kotlin,
       java,
       go,
       regex,
       docker,
       wireguard,
-      gcp,
+      computeEngine,
+      vultr,
       git,
-      android,
+      rpi,
+      android
     ],
-    8
+    6
   );
 
   return (
