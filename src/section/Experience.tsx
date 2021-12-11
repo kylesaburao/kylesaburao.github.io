1b8b9fe3ca412b16087500277896167598179e27
Kyle Saburao <ksaburao@sfu.ca>
2021-12-11 11:29:25 -0800

    refactor experience

--- src/section/Experience.tsx
+++ src/section/Experience.tsx
@@ -3,24 +3,38 @@ import { Container, Col, Row } from "react-bootstrap";
 
 import CardItem from "../CardItem";
 
+interface WorkExperience {
+  title: string;
+  description: string;
+  date: string;
+}
+
+function MakeWorkExperience(
+  title: string,
+  description: string,
+  date: string
+): WorkExperience {
+  return { title, description, date };
+}
+
 export default function Experience() {
-  const experience = [
-    {
-      title: "SAP",
-      description: "iXp Intern - Concur Travel Software Engineer",
-      date: "September 2021 - Now",
-    },
-    {
-      title: "NETINT Technologies Inc.",
-      description: "Co-op Firmware Engineer",
-      date: "January 2020 (8 months)",
-    },
+  const experiences: WorkExperience[] = [
+    MakeWorkExperience(
+      "SAP",
+      "iXp Intern - Concur Travel Software Engineer",
+      "September 2021 - Now"
+    ),
+    MakeWorkExperience(
+      "NETINT Technologies Inc.",
+      "Co-op Firmware Engineer",
+      "January 2020 (8 months)"
+    ),
   ];
 
   return (
     <Container>
       <Row className="justify-content-center">
-        {experience.map((item) => (
+        {experiences.map((item) => (
           <Col key={item.title} className="d-flex justify-content-around">
             <CardItem
               title={item.title}
