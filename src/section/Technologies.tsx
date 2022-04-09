fb03108e5ca1c9ea52681942582e472e150779df
Kyle Saburao <ksaburao@sfu.ca>
2022-04-08 23:02:18 -0700

    update details

--- src/section/Technologies.tsx
+++ src/section/Technologies.tsx
@@ -3,21 +3,22 @@ import { Col, Container, Row } from "react-bootstrap";
 import android from "../img/icons/android.svg";
 import angular from "../img/icons/angular.svg";
 import cpp from "../img/icons/c++.svg";
+import computeEngine from "../img/icons/compute_engine.svg";
 import docker from "../img/icons/docker.png";
 import git from "../img/icons/git.svg";
 import go from "../img/icons/go.svg";
+import graphql from "../img/icons/graphql.svg";
 import java from "../img/icons/java.svg";
 import javascript from "../img/icons/javascript.png";
+import kotlin from "../img/icons/kotlin.svg";
 import python from "../img/icons/python.svg";
 import react from "../img/icons/react.svg";
+import redux from "../img/icons/redux.svg";
 import regex from "../img/icons/regex.svg";
+import rpi from "../img/icons/rpi.svg";
 import typescript from "../img/icons/typescript.svg";
+import vultr from "../img/icons/vultr.svg";
 import wireguard from "../img/icons/wireguard.svg";
-import redux from "../img/icons/redux.svg"
-import kotlin from "../img/icons/kotlin.svg"
-import computeEngine from "../img/icons/compute_engine.svg"
-import rpi from "../img/icons/rpi.svg"
-import vultr from "../img/icons/vultr.svg"
 import chunkArrayInGroups from "../manip/arrayChunker";
 
 export default function Technologies() {
@@ -25,6 +26,7 @@ export default function Technologies() {
     [
       react,
       redux,
+      graphql,
       angular,
       typescript,
       javascript,
@@ -40,9 +42,9 @@ export default function Technologies() {
       vultr,
       git,
       rpi,
-      android
+      android,
     ],
-    6
+    5
   );
 
   return (
